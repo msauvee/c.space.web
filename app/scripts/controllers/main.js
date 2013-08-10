0ae0fb12f5bbf1fcbc12f7c8a3414935fcb51702
msauvee <mickael.sauvee@free.fr>
2013-08-10 14:49:31 +0200

    infa

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('c.space.webApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
